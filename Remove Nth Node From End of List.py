dc58be10e85dc7ccaad3c161aa7fdf5f2b436a83
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-04-10 18:37:43 -0700

    Add files via upload

--- Remove Nth Node From End of List.py
+++ Remove Nth Node From End of List.py
@@ -24,3 +24,24 @@ class Solution:
         #delete
         left.next = left.next.next
         return dummy.next
+
+        #One Pointer
+    #   ptr, length = head, 0
+	#   while ptr:
+	# 	    ptr, length = ptr.next, length + 1
+	#   if length == n : return head.next
+	#   ptr = head
+	#   for i in range(1, length - n):
+	# 	    ptr = ptr.next
+	#   ptr.next = ptr.next.next
+	#   return head
+
+        #Two Pointer
+        # fast = slow = head
+	    # for i in range(n):
+		#     fast = fast.next
+	    # if not fast: return head.next
+	    # while fast.next:
+		#     fast, slow = fast.next, slow.next
+	    # slow.next = slow.next.next
+	    # return head
